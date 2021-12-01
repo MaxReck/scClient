896387791c225544384549089bd6c61067a876ea
MaxReck <93326113+MaxReck@users.noreply.github.com>
2021-12-01 11:27:45 +0100

    schule

--- src/main/sc/player2022/logic/Evaluate.java
+++ src/main/sc/player2022/logic/Evaluate.java
@@ -8,9 +8,20 @@ public class Evaluate {
     public static float rateGameState(GameState gameState, Team team) {
         float rating = 0.0f;
         rating += gameState.getPointsForTeam(team) - gameState.getPointsForTeam(gameState.getOtherTeam());
-
+        
 
 
         return rating;
     }
+
+    private static int getPointsForPieces(Team team, GameState gameState) {
+        float rating = 0;
+        Map<Piece> pices = new ArrayList(gameState.getPices(team));
+        for(Pices pice: pcies) {
+            //String name= pice.getName();
+            //if (name == möwe) rating += 
+        } 
+
+        
+    }         
 }
